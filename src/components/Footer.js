804107844aaefc5e2dcba31378bdc8decb95d13c
kristihu <44356817+kristihu@users.noreply.github.com>
2019-09-09 11:18:59 +0300

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+
+render() {
+  return(
+    <React.Fragment>
+    <div className="Footer">
+      <p>Copyright 2020</p>
+    </div>
+    </React.Fragment>
+  );
+}
+
+}
+
+export default Footer;
