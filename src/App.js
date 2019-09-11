082a06a6cd54327eb47e5db6a557000a773dd0ff
kristihu <44356817+kristihu@users.noreply.github.com>
2019-09-11 13:51:15 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,71 +1,31 @@
-import React, {Component} from 'react';
-import Nav from './components/Nav';
-import TimeTable from './components/TimeTable';
-import VideoContainer from './components/VideoContainer';
-import Footer from './components/Footer';
+import React from 'react';
+import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
+
 import './App.css';
+import Main from './components/main';
+import {Link} from 'react-router-dom';
 
-class App extends Component {
+function App() {
+  return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
 
-  constructor(props) {
-    super(props);
-     this.state = {
-        width: window.innerWidth,
-        height: window.innerHeight,
-        video: {
-          width: "600px",
-          height: "400px",
-        }
-       };
-     this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-     this.setVideoSceenSize = this.setVideoSceenSize.bind(this);
-   }
-
-   componentDidMount() {
-     this.updateWindowDimensions();
-     window.addEventListener('resize', this.updateWindowDimensions);
-   }
-
-   componentWillUnmount() {
-     window.removeEventListener('resize', this.updateWindowDimensions);
-   }
-
-   updateWindowDimensions() {
-     this.setState({ width: window.innerWidth, height: window.innerHeight });
-     this.setVideoSceenSize();
-   }
-
-   setVideoSceenSize() {
-     //muuta videon kokoa kun näytön leveys menee alle 800
-     if(this.state.width < 850){
-       if(this.state.width < 400){ //mobiilinäkymä
-         console.log(this.state.width);
-         this.state.video.width = this.state.width;
-       }else{
-       this.state.video.width = "400px";
-       this.state.video.height = "300px";
-      }
-     }else{
-       this.state.video.width = "600px";
-       this.state.video.height = "400px";
-     }
-     this.forceUpdate(); //Jotta state.video päivyttyy
-   }
-
-render() {
-
-    return (
-        <div className="App">
-            <Nav />
-            <TimeTable />
-            <VideoContainer
-              video={this.state.video}
-              asd={"asdsadsd"}
-            />
-            <Footer />
-        </div>
-    );
-  }
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/landingpage">KOTI</Link>
+              <Link to="/">Link</Link>
+              <Link to="/">Link</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main/>
+          </Content>
+        </Layout>
+      </div>
+  );
 }
 
 export default App;
