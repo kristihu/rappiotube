a37d91ba13ada02809a330013e1c8e0c0fd9cd91
jakasco <kasperi.mutku@elisanet.fi>
2019-09-11 14:35:27 +0300

    Nav lisätty vanhaan

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-
 import './App.css';
 import Main from './components/main';
 import {Link} from 'react-router-dom';
+import Nav from './components/Nav';
+import TimeTable from './components/TimeTable';
+import VideoContainer from './components/VideoContainer';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+  super(props);
+   this.state = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      video: {
+        width: "600px",
+        height: "400px",
+      }
+     };
+   this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+   this.setVideoSceenSize = this.setVideoSceenSize.bind(this);
+ }
+
+ componentDidMount() {
+   this.updateWindowDimensions();
+   window.addEventListener('resize', this.updateWindowDimensions);
+ }
+
+ componentWillUnmount() {
+   window.removeEventListener('resize', this.updateWindowDimensions);
+ }
+
+ updateWindowDimensions() {
+   this.setState({ width: window.innerWidth, height: window.innerHeight });
+   this.setVideoSceenSize();
+ }
+
+ setVideoSceenSize() {
+   //muuta videon kokoa kun näytön leveys menee alle 800
+   if(this.state.width < 850){
+     if(this.state.width < 400){ //mobiilinäkymä
+       console.log(this.state.width);
+       this.state.video.width = this.state.width;
+     }else{
+     this.state.video.width = "400px";
+     this.state.video.height = "300px";
+    }
+   }else{
+     this.state.video.width = "600px";
+     this.state.video.height = "400px";
+   }
+   this.forceUpdate(); //Jotta state.video päivyttyy
+ }
+
+render(){
   return (
       <div className="demo-big-content">
         <Layout>
-          <Header title="Title" scroll>
+          <Header title="RappioTube" scroll>
 
           </Header>
-          <Drawer title="Title">
+          <Drawer title="Valikko">
             <Navigation>
               <Link to="/landingpage">KOTI</Link>
               <Link to="/">Link</Link>
@@ -24,8 +72,15 @@ function App() {
             <Main/>
           </Content>
         </Layout>
+        <Nav />
+            <TimeTable />
+            <VideoContainer
+              video={this.state.video}
+              asd={"asdsadsd"}
+            />
       </div>
   );
+}
 }
 
 export default App;
